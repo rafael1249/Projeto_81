2ad33f6653b466ea5cca4b57b16ed6fffc144921
Rafael dos Anjos <89105388+rafael1249@users.noreply.github.com>
2023-10-06 18:13:02 -0300

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import {View, Text, StyleShet } from "react-native";
+
+export default class Profile extends Component  {
+
+    render(){ 
+    
+      return(
+       <View>
+
+        <Text>Tela Profile</Text>
+       </View>
+
+      );
+    }
+
+}
